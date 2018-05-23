a9017b52c6060d4fae59466048b2f0e3df88ded5
Claudio Biancalana <claudio.biancalana@gmail.com>
2018-05-23 12:01:08 +0200

    aggiornamento nome progetto

--- conf.py
+++ conf.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # -- PROJECT Variables ----------------------------------------------------
-settings_project_name = "Linee Guida di Comunicazione"
+settings_project_name = "Linee guida per la promozione dei servizi digitali"
 settings_copyright_copyleft = "Agenzia per l'Italia Digitale"
 settings_editor_name = "Agenzia per l'Italia Digitale"
 settings_doc_version = 'Bozza in consultazione'
